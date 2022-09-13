f4ff9c8c84dec17568f0a23794a7dcca935c9a51
Gidoiu Alex <gidoiualex@yahoo.com>
2022-09-13 14:50:08 +0300

    DONE

---
+++ index.js
@@ -0,0 +1,49 @@
+var numberOfButtons = 
+    document.querySelectorAll(".button").length;
+  
+for (var j = 0; j < numberOfButtons; j++) {
+  
+  document.querySelectorAll(".button")[j]
+  .addEventListener("click", function() {
+  
+    var buttonStyle = this.innerHTML;
+    sound(buttonStyle);
+    animation(buttonStyle);
+  });
+}
+  
+document.addEventListener("keypress", function(event) {
+  sound(event.key);
+  animation(event.key);
+});
+  
+function sound(key) {
+  switch (key) {
+    case "w":
+      var sound1 = new Audio("sound/sounds_crash.mp3");
+      sound1.play();
+      break;
+  
+    case "a":
+      var sound2 = new Audio("sound/sounds_tom-2.mp3");
+      sound2.play();
+      break;
+  
+    case "s":
+      var sound3 = new Audio("sound/sounds_tom-1.mp3");
+      sound3.play();
+      break;
+  
+    case "d":
+      var sound4 = new Audio("sound/sounds_snare.mp3");
+      sound4.play();
+      break;
+  
+    case "l":
+      var sound5 = new Audio("sound/sounds_kick-bass.mp3");
+      sound5.play();
+      break;
+
+    default: console.log(key);
+  }
+}
